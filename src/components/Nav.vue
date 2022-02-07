3826663b8a8bd2888562225208d92d288a386c30
Caelum Dudek <caelumdudek25@gmail.com>
2022-02-06 18:22:01 -0800

    Added bootstrap 5
    Added bootstrap 5

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,30 +1,11 @@
 <template>
-  <nav class="top bg-gray-900 text-white py-4 px-6 flex flex-row">
-    <div class="basis-1/4">
-      <h1 class="branding">RateMyCoop</h1>
-    </div>
-    <div class="basis-3/4 text-right">
-      <button
-        class="nav-btn bg-yellow-200 text-black rounded-full shadow-sm hover:bg-yellow-300 w-14 h-14 p-2"
-      >
-        <i class="fa fa-search" style="font-size: 2.5em;"></i>
-      </button>
-      <button
-        class="nav-btn bg-yellow-200 text-black rounded-full shadow-sm hover:bg-yellow-300 w-14 h-14 p-2 mx-4"
-      >
-        <i class="fa fa-plus" style="font-size: 2.5em;"></i>
-      </button>
-      <button
-        class="nav-btn bg-yellow-200 text-black rounded-full shadow-sm hover:bg-yellow-300 w-14 h-14 p-2"
-      >
-        <i class="fa fa-user" style="font-size: 2.5em;"></i>
-      </button>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <router-link class="navbar" :to="{ name: '', params: {} }">Home</router-link>
     </div>
   </nav>
 </template>
 
 <style>
-@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
-@import "https://fonts.googleapis.com/css?family=Noto Sans";
-@import "https://cdn.tailwindcss.com";
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 
 createApp(App).use(store).use(router).mount('#app')
