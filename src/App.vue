230a9a253eb7fa3021392bf0794edce378f49e68
Caelum Dudek <caelumdudek25@gmail.com>
2022-02-17 19:58:25 -0800

    Update App.vue
    Nav and footer working

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <Navbar />
   <router-view />
   <Footer />
 </template>
 
 <script>
 import Footer from "@/components/Footer.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: "app",
   components: {
     Footer,
+    Navbar,
   },
 };
 </script>
