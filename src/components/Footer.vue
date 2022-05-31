7fd1b721ec0cc978d944828e7ad5ad01e1ceea8a
Amanda Anderson <aganderson@shaw.ca>
2022-05-30 19:45:18 -0700

    Fix indentation in .vue files

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,21 +2,13 @@
   <footer class="bg-dark fixed-bottom main-footer">
     <!-- Does not wrap -->
     <p>
-      <router-link class="footer-link" :to="{ name: '', params: {} }"
-        >About Us</router-link
-      >
+      <router-link class="footer-link" :to="{ name: '', params: {} }">About Us</router-link>
       <span class="spacer"></span>
-      <router-link class="footer-link" :to="{ name: '', params: {} }"
-        >Vikelabs</router-link
-      >
+      <router-link class="footer-link" :to="{ name: '', params: {} }">Vikelabs</router-link>
       <span class="spacer"></span>
-      <router-link class="footer-link" :to="{ name: '', params: {} }"
-        >Github</router-link
-      >
+      <router-link class="footer-link" :to="{ name: '', params: {} }">Github</router-link>
       <span class="spacer"></span>
-      <router-link class="footer-link" :to="{ name: '', params: {} }"
-        >Help</router-link
-      >
+      <router-link class="footer-link" :to="{ name: '', params: {} }">Help</router-link>
     </p>
   </footer>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <router-link class="navbar-brand" :to="{ name: '', params: {} }"
-        >RateMyCoop</router-link
-      >
+      <router-link class="navbar-brand" :to="{ name: '', params: {} }">RateMyCoop</router-link>
       <button
         class="navbar-toggler"
         type="button"
@@ -11,8 +9,7 @@
         data-bs-target="#nav-links"
         aria-controls="nav-links"
         aria-expanded="false"
-        aria-label="Toggle the Navbar in mobile"
-      >
+        aria-label="Toggle the Navbar in mobile">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="nav-links">
@@ -20,45 +17,37 @@
           <li class="nav-item ms-auto" id="search-item">
             <span>
               <router-link
-                class="nav-link darken"
-                :to="{ name: '', params: {} }"
-                title="Search"
-                ><p class="display-on-small nav-text" id="tooltip">Search</p>
+                class="nav-link darken" :to="{ name: '', params: {} }"
+                title="Search">
+                <p class="display-on-small nav-text" id="tooltip">Search</p>
                 <img
                   class="nav-btn"
                   src="@/assets/nav_assets/search.svg"
-                  alt="Search"
-                />
+                  alt="Search"/>
               </router-link>
             </span>
           </li>
           <li class="nav-item ms-auto" id="add-review-item">
             <span>
               <router-link
-                class="nav-link darken"
-                :to="{ name: '', params: {} }"
-                title="Add Review"
-                ><p class="display-on-small nav-text">Add Review</p>
+                class="nav-link darken" :to="{ name: '', params: {} }" title="Add Review">
+                <p class="display-on-small nav-text">Add Review</p>
                 <img
                   class="nav-btn"
                   src="@/assets/nav_assets/add_review.svg"
-                  alt="Add Review"
-                />
+                  alt="Add Review"/>
               </router-link>
             </span>
           </li>
           <li class="nav-item ms-auto" id="profile-item">
             <span>
               <router-link
-                class="nav-link darken"
-                :to="{ name: '', params: {} }"
-                title="Profile"
-                ><p class="display-on-small nav-text">Profile</p>
+                class="nav-link darken" :to="{ name: '', params: {} }" title="Profile">
+                <p class="display-on-small nav-text">Profile</p>
                 <img
                   class="nav-btn"
                   src="@/assets/nav_assets/profile.svg"
-                  alt="Profile"
-                />
+                  alt="Profile"/>
               </router-link>
             </span>
           </li>
