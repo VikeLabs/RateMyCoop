bcd647d289bfc7efbd2861408c058a5bc702004d
Tom Chapman <tchapman000@gmail.com>
2022-05-30 19:21:11 -0700

    FAQ_page

---
+++ src/components/QuestionAndAnswer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container w-50 pb-5">
+    <div class="row">
+      <div class="col-sm h4 bg-dark rounded-pill p-4 my-auto">
+        <div>
+          {{ question }}
+        </div>
+      </div>
+      <div class="col-sm h5">
+        {{ answer }}
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  props: ['question', 'answer']
+}
+
+</script>
