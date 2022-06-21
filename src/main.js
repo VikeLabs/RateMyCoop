5d754f8b310d07b4d5acad4c392df069a173ceb1
Amanda Anderson <aganderson@shaw.ca>
2022-06-20 19:37:01 -0700

    Add style.css
    Add style.css and make sure it's served when the app is run. Also consistent quotations marks for import statements

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/style.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-import "@popperjs/core";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '@popperjs/core';
 
 
 createApp(App).use(store).use(router).mount("#app");
